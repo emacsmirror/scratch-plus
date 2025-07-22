4d30149c820c595c71ba75a7a65561b1a2f988f9
Samuel W. Flint <swflint@flintfam.org>
2025-07-21 19:48:31 -0500

    Fix a bunch of bugs & re-release

--- scratch-plus.el
+++ scratch-plus.el
@@ -5,7 +5,7 @@
 ;; Author: Samuel W. Flint <swflint@flintfam.org>
 ;; Keywords: convenience
 ;; Homepage: https://git.sr.ht/~swflint/scratch-plus
-;; Version: 1.0.0
+;; Version: 1.1.0
 ;; Package-Requires: ((emacs "29.1"))
 
 ;; This program is free software; you can redistribute it and/or modify
